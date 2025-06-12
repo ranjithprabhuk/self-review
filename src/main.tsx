2cd51727b9ee731653afb07f5e36f3f8ce27e843
Ranjithprabhu K <ranjithprabhu.k@gmail.com>
2025-06-12 14:31:27 +0530

    fix: ignore the ts issue

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* @ts-ignore */}
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
       <App />
     </MantineProvider>
